2c385e837ec9635597c5f3591b43719ae4339ba1
Faisal <peysal@gmail.com>
2017-10-08 18:34:56 +0800

    added provider . Encapsulate all its inner comp.

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
+import { Provider } from 'react-redux';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,17 +24,19 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Provider store={}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit App.js
+          </Text>
+          <Text style={styles.instructions}>
+            {instructions}
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
