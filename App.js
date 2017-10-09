138166b92b50a87696549a2989632e41d6f4e26d
Faisal <mohammad.faisal.poat@rhbgroup.com>
2017-10-09 16:06:17 +0800

    Move view component to specific folder

--- App.js
+++ App.js
@@ -26,37 +26,10 @@ export default class App extends Component<{}> {
   render() {
     return (
       <Provider store={rootStore}>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          <Text style={styles.instructions}>
-            {instructions}
-          </Text>
-        </View>
+        
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
