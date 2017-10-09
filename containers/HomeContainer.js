c1a539dd01885911c2424e3020f1f50b2543e1ba
Faisal <mohammad.faisal.poat@rhbgroup.com>
2017-10-09 16:23:36 +0800

    reducer to change state in store

--- containers/HomeContainer.js
+++ containers/HomeContainer.js
@@ -8,8 +8,8 @@ const mapStateToProps = rootState => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    action1:() => {dispatch({ type:'action1' })} , 
-    action2:() => {dispatch({ type: 'action2'})}
+    action1:() => {dispatch({ type:'action1'})} , 
+    action2:() => {dispatch({ type:'action2'})}
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeView)
--- store/root.js
+++ store/root.js
@@ -1,6 +1,15 @@
 import {createStore} from 'redux'
 
-export const rootState = () => {}
+export const rootState = (state, action) => {
+    switch (action.type) {
+        case 'action1': 
+            return 'yo action1';
+        case 'action2':
+            return 'yes action2';
+        default:
+            return state;
+    }
+}
 
 let rootStore = createStore(rootState);
 
