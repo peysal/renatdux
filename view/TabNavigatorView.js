6ae1666489a5c1f63ee2d85434dc2dfbfb4d4dfe
Faisal <mohammad.faisal.poat@rhbgroup.com>
2017-10-18 22:20:33 +0800

    prevent tab from showing blank!

--- view/TabNavigatorView.js
+++ view/TabNavigatorView.js
@@ -36,7 +36,7 @@ const TabNavigation = TabNavigator({
 class TabNavigatorView extends React.Component {
     render() {
         return (
-            <View>
+            <View style={{flex: 1}}>
                 <TabNavigation navigation={this.props.navigation}/>
             </View>
         );
