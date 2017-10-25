1df7fe775c1585d09d72c7c1e8d3709a2ed5ed56
Faisal <mohammad.faisal.poat@rhbgroup.com>
2017-10-25 22:53:32 +0800

    Eslint in action

--- App.js
+++ App.js
@@ -4,19 +4,15 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import rootStore from './store/root.js';
-import HomeContainer from './containers/HomeContainer.js';
-import RootNavigatorView from './view/RootNavigatorView.js';
-import AppView from './view/AppView.js';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import rootStore from "./store/root.js";
+import HomeContainer from "./containers/HomeContainer.js";
+import RootNavigatorView from "./view/RootNavigatorView.js";
+import AppView from "./view/AppView.js";
 
-export default class App extends Component<{}> {
+export default class App extends React.Component {
   render() {
-    return (
-
-        <AppView />
-
-    );
+    return <AppView />;
   }
 }
