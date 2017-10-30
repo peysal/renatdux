1e6817f3b91423fa201045e029b121ec2afdf839
Faisal <mohammad.faisal.poat@rhbgroup.com>
2017-10-30 18:13:33 +0800

    drawer 1 and drawer 2 with different type navigation. Apply formatting

--- view/AppView.js
+++ view/AppView.js
@@ -1,68 +1,79 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { DrawerNavigator, TabNavigator, StackNavigator} from 'react-navigation';
+import { DrawerNavigator, TabNavigator, StackNavigator } from 'react-navigation';
 
 class MainScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Hello Main</Text>
-            </View> 
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello Main</Text>
+      </View>
+    );
+  }
 }
 
 class DetailScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Hello Detail</Text>
-            </View> 
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello Detail</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 const stackNavigator = StackNavigator({
-    Main: {
-        screen: DetailScreen
-    },
-    Detail: {
-        screen: DetailScreen
-    }
+  Main: {
+    screen: DetailScreen,
+  },
+  Detail: {
+    screen: DetailScreen,
+  },
 });
 
-const tabNavigator = TabNavigator({
+const tabNavigator1 = TabNavigator(
+  {
     TabItem1: {
-        screen: stackNavigator,
-        navigationOptions: {
-            tabBarLabel: 'Tab 1'
-        } 
-    }
-}, {
+      screen: stackNavigator,
+      navigationOptions: {
+        tabBarLabel: 'Tab 1',
+      },
+    },
+  },
+  {
     tabBarPosition: 'bottom',
     animationEnabled: true,
     tabBarOptions: {
-      activeTintColor: '#e91e63'
-    }
-  });
+      activeTintColor: '#e91e63',
+    },
+  },
+);
 
 const drawerNavigator = DrawerNavigator({
-    DrawerItem1: {
-        screen: tabNavigator,
-        navigationOptions: {
-            drawer: {
-                label: 'drawer 1'
-            }
-        }
-    }
+  DrawerItem1: {
+    screen: stackNavigator,
+    navigationOptions: {
+      drawer: {
+        label: 'drawer 1',
+      },
+    },
+  },
+  DrawerItem2: {
+    screen: tabNavigator1,
+    navigationOptions: {
+      drawer: {
+        label: 'drawer 2',
+      },
+    },
+  },
 });
 
 export default drawerNavigator;
